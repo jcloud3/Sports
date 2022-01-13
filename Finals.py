c42c93ba68983632eb8a6a00af4119b8ca748c97
John Cloud <john.cloud3@gmail.com>
2022-01-13 15:09:38 -0600

    total work

--- Finals.py
+++ Finals.py
@@ -1,4 +1,18 @@
-from sportsipy.ncaab.boxscore import Boxscore
+from datetime import datetime, timedelta
+from sportsipy.nba.boxscore import Boxscores
+import pandas
+sport = 'basketball_nba'
+yesterday = datetime.today()- timedelta(days = 1)
+games_today = Boxscores(yesterday)
+date = yesterday.strftime('%-m-%d-%Y')
+print (games_today.games.keys())
+df = pandas.DataFrame.from_dict(games_today.games[date]).drop(['boxscore','losing_abbr','losing_name','winning_abbr','winning_name','home_abbr','away_abbr'],axis=1)
+df['total_score'] = df['away_score'] + df['home_score']
+print(df)
+fileName = sport+yesterday.strftime('%Y-%m-%d')+'.csv'
+oddsDF = pandas.read_csv(fileName).drop(columns='Unnamed: 0')
 
-championship_game = Boxscore('2018-01-08-georgia')
-print(championship_game.away_interceptions)
+#print(oddsDF.loc[oddsDF['home_team'].str.contains('Brooklyn', case=False)])
+for team in df:
+    print(team)
+    oddsDF[oddsDF.loc[oddsDF['home_team'].str.contains(team.loc['home_name'], case=False)],'total_score'] = team.loc['total_score']
