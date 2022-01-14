ff6632cbd550474c1d07f7aec6a2dcbddcb2eee6
John Cloud <john.cloud3@gmail.com>
2022-01-14 10:33:05 -0600

    Update file names and add code to calc w/l

---
+++ AnalyzePlays.py
@@ -0,0 +1,41 @@
+import pandas
+from datetime import datetime, timedelta
+
+THRESHHOLD = 3
+
+
+
+def CheckBets(sport, num):
+    results = pandas.DataFrame([[sport,0,0,0,0.0]],columns=['sport','win','loss','push','price'])
+    if num>1:
+        yesterday = (datetime.today()- timedelta(days = 1)).strftime('%Y-%m-%d')
+        fileName = sport+yesterday+".csv"
+        oddsDF = pandas.read_csv(fileName).drop(columns='Unnamed: 0')
+        overs = oddsDF.loc[oddsDF['name'].str.contains("Over", case=False)]
+        #oddsDF['point'] - oddsDF[f'point{num}'] > THRESHHOLD and oddsDF['name'] == "Over"
+        unders = oddsDF.loc[oddsDF['name'].str.contains("Under", case=False)]
+        
+        overs = overs[overs['point'] - overs[f'point{num}'] > THRESHHOLD]
+        unders = unders[unders['point']-unders[f'point{num}'] <- THRESHHOLD]
+        
+        overs = overs[overs['bookmakers.title'].str.contains('BetRivers',case=False)]
+        unders = unders[overs['bookmakers.title'].str.contains('BetRivers',case=False)]
+        overs['result'] = overs[f'point{num}']<overs['total_score']
+        results['win'] = overs.result.sum()
+        results['loss'] = (overs[f'point{num}']>overs['total_score']).sum()
+        results['push'] = (overs[f'point{num}']==overs['total_score']).sum()
+        unders['result'] = unders[f'point{num}']>overs['total_score']
+        results['win'] += unders.result.sum()
+        results['loss'] += (unders[f'point{num}']<unders['total_score']).sum()
+        results['push'] = (unders[f'point{num}']==overs['total_score']).sum()
+        
+        
+        oddsDF = pandas.concat([overs,unders])
+        #need to fix this. Should only get price of winning bets
+        results['price'] = oddsDF['price'].mean()
+        print(oddsDF)
+        
+        print(results)
+        fileName = sport + 'BETS.csv'
+        #oddsDF.to_csv(fileName)
+CheckBets('basketball_nba',2)
--- Runner.py
+++ Runner.py
@@ -1,6 +1,6 @@
 import Gambler
 import FindBets
-runNum = 2
+runNum = 5
 Gambler.getOdds('basketball_ncaab',runNum)
 Gambler.getOdds('basketball_nba',runNum)
 FindBets.CheckBets('basketball_ncaab',runNum)
