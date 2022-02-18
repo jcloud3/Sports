5444e2c38b079c38a6b0eec73bf8e2e7f568d5e1
John Cloud <john.cloud3@gmail.com>
2022-02-18 09:45:42 -0600

    wont find bets if no games played

--- FindBets.py
+++ FindBets.py
@@ -2,6 +2,7 @@ from unicodedata import name
 import pandas
 from datetime import date
 import smtplib, ssl
+import os.path
 
 THRESHHOLD = 3.5
 def SendEmail(bets):
@@ -27,25 +28,26 @@ def SendEmail(bets):
 def CheckBets(sport):
     today = str(date.today())
     fileName = sport+today+".csv"
-    oddsDF = pandas.read_csv(fileName).drop(columns='Unnamed: 0')
-    #previousBets = pandas.read_csv(sport + 'BETS.csv').drop(columns='Unnamed: 0')
-    num = int(((len(oddsDF.columns)-5)/2))
-    if num>1:
-        overs = oddsDF.loc[oddsDF['name'].str.contains("Over", case=False)]
-        #oddsDF['point'] - oddsDF[f'point{num}'] > THRESHHOLD and oddsDF['name'] == "Over"
-        unders = oddsDF.loc[oddsDF['name'].str.contains("Under", case=False)]
-        
-        overs = overs[overs['point'] - overs[f'point{num}'] > THRESHHOLD]
-        unders = unders[unders['point']-unders[f'point{num}'] <- THRESHHOLD]
-        
-        
-        
-        oddsDF = pandas.concat([overs,unders],ignore_index=True)
-        print(oddsDF)
-       # if len(oddsDF) > 0:
-           # SendEmail(oddsDF)
-        fileName = sport + 'BETS.csv'
-        oddsDF.to_csv(fileName)
+    if (os.path.exists(fileName)):
+        oddsDF = pandas.read_csv(fileName).drop(columns='Unnamed: 0')
+        #previousBets = pandas.read_csv(sport + 'BETS.csv').drop(columns='Unnamed: 0')
+        num = int(((len(oddsDF.columns)-5)/2))
+        if num>1:
+            overs = oddsDF.loc[oddsDF['name'].str.contains("Over", case=False)]
+            #oddsDF['point'] - oddsDF[f'point{num}'] > THRESHHOLD and oddsDF['name'] == "Over"
+            unders = oddsDF.loc[oddsDF['name'].str.contains("Under", case=False)]
+            
+            overs = overs[overs['point'] - overs[f'point{num}'] > THRESHHOLD]
+            unders = unders[unders['point']-unders[f'point{num}'] <- THRESHHOLD]
+            
+            
+            
+            oddsDF = pandas.concat([overs,unders],ignore_index=True)
+            print(oddsDF)
+        # if len(oddsDF) > 0:
+            # SendEmail(oddsDF)
+            fileName = sport + 'BETS.csv'
+            oddsDF.to_csv(fileName)
 #CheckBets('basketball_ncaab')
 
 
